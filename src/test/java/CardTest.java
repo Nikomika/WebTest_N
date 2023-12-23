36e0ca7f04a0de4b3ebe0069e176ac285e65fbaf
OlgaNiki <oliadrok@gmail.com>
2023-12-23 13:26:28 +0300

    file correction

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CardTest {
@@ -24,7 +26,14 @@ public class CardTest {
         driver = null;
     }
     @Test
-    void shouldTestSomething(){
+    void shouldTestSomething() throws InterruptedException {
         driver.get("http://0.0.0.0:9999");
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys ("Nataliya");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys ("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+        Thread.sleep(5000);
+
     }
 }
